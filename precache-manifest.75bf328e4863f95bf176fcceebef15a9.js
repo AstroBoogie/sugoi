5c5ce41a0cfdf705335cc8aa02b650210e7fde79
AstroBoogie <nathaniel.adams.us@gmail.com>
2019-11-26 18:46:01 -0800

    Updates

---
+++ precache-manifest.75bf328e4863f95bf176fcceebef15a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e96c4b535bf69a2b90b7bad57d1d3b26",
+    "url": "/sugoi/index.html"
+  },
+  {
+    "revision": "033ed9403085d83dab28",
+    "url": "/sugoi/static/css/main.5679962c.chunk.css"
+  },
+  {
+    "revision": "597d3d0735fca5f42210",
+    "url": "/sugoi/static/js/2.693e8094.chunk.js"
+  },
+  {
+    "revision": "033ed9403085d83dab28",
+    "url": "/sugoi/static/js/main.d96b2bd0.chunk.js"
+  },
+  {
+    "revision": "c77a408081a22471657c",
+    "url": "/sugoi/static/js/runtime-main.b7af68a4.js"
+  }
+]);
